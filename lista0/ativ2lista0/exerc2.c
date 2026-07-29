d47c5ad880422fd49f868100aae4015491e2d82c
CaioCosta23 <caio.c.lopes>
2026-07-29 17:53:14 -0300

    Adicionando exercício 2

---
+++ lista0/ativ2lista0/exerc2.c
@@ -0,0 +1,13 @@
+// Faça um programa que leia dois valores inteiros e imprima-os na tela de maneira inversa ao seu recebimento;
+
+#include <stdio.h>
+
+int main() {
+	int numero1, numero2;
+	
+	scanf("%d %d", &numero1, &numero2);
+	
+	printf("%d %d\n", numero2, numero1);
+	
+	return 0;
+}
