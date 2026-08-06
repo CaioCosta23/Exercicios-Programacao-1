baf7552eb93edb8ae6f3411b5a4a3eb58ffed5ad
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-08-06 19:19:37 -0300

    Adicionando código do programa

---
+++ lista5/lista5Atividade4/exercicio4.c
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   exercicio4.c
+ * Author: Caio Costa Lopes
+ *
+ * Created on 6 de agosto de 2026, 18:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void leNumeros(int numeros[], int quantidadeNumeros);
+void imprimeValoresDentroForaIntervalo(int numeros[], int quantidadeNumeros, int inicioIntervalo, int fimIntervalo);
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int quantidadeNumeros, inicioIntervalo, fimIntervalo;
+    
+    scanf("%d", &quantidadeNumeros);
+    
+    int numeros[quantidadeNumeros];
+    
+    leNumeros(numeros, quantidadeNumeros);
+    
+    scanf("%d %d", &inicioIntervalo, &fimIntervalo);
+    
+    imprimeValoresDentroForaIntervalo(numeros, quantidadeNumeros, inicioIntervalo, fimIntervalo);
+
+    return (EXIT_SUCCESS);
+}
+
+void leNumeros(int numeros[], int quantidadeNumeros) {
+    int n;
+    
+    for(n = 0; n < quantidadeNumeros; n++) {
+        scanf("%d", &numeros[n]);
+    }
+}
+
+void imprimeValoresDentroForaIntervalo(int numeros[], int quantidadeNumeros, int inicioIntervalo, int fimIntervalo) {
+    int n, i;
+    int dentro = 0, fora = 0;
+    int achado;
+    
+    
+    for(n = 0; n < quantidadeNumeros; n++) {
+        achado = 0;
+        for(i = inicioIntervalo; i <= fimIntervalo; i++) {
+            if (numeros[n] == i){
+                achado = 1;
+                break;
+            }
+        }
+        if (achado)
+            dentro += 1;
+        else
+            fora += 1;
+    }
+    
+    printf("%d %d", dentro, fora);
+}
+
